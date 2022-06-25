a7c7d597805444b51cf6efa15eec5da16533b966
Yukio Hirayama <suugakun@gmail.com>
2022-06-25 22:19:02 +0900

    1-1-2

---
+++ chapter01/1-1-2.scm
@@ -0,0 +1,10 @@
+#!/usr/local/bin/gosh
+
+(define size 2)
+(print size)
+(print (* 5 size))
+(define pi 3.14159)
+(define radius 10)
+(print (* pi (* radius radius)))
+(define circumference (* 2 pi radius))
+(print circumference)
